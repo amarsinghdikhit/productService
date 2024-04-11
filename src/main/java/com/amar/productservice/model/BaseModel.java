7256519ff3b70d865f86f2a23f842fd033baa3b2
Amar Singh <amar.jrschbs@gmail.com>
2024-04-11 10:20:18 +0530

    changes for mapping, dependency for sql and data JPA

---
+++ src/main/java/com/amar/productservice/model/BaseModel.java
@@ -0,0 +1,23 @@
+package com.amar.productservice.model;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseModel {
+
+    @Id
+    private Long id;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+
+    private boolean isDeleted;
+}
--- src/main/java/com/amar/productservice/model/Product.java
+++ src/main/java/com/amar/productservice/model/Product.java
@@ -1,18 +1,21 @@
 package com.amar.productservice.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Product {
-
-    private Long id;
+@Entity
+public class Product extends BaseModel{
 
     private String title;
 
     private Double price;
 
+    @ManyToOne
     private Category category;
 
     private String description;
